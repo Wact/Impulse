2ddd648499669137bca407a9bd16a3791bf1aee5
Wact <malygin.wm@gmail.com>
2017-07-06 01:05:23 +0300

    MainScene

---
+++ Assets/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+
+        transform.parent.GetComponent<rotate1>().isRotating = false;
+        other.transform.parent.GetComponent<rotate1>().isRotating = true;
+        //other.gameObject.GetC<rotate1>().isRotating = false;
+        //other.gameObject.GetComponent<rotate1>().isRotating = false;
+        Debug.Log("Пересеклись");
+    }
+}
---
+++ Assets/rotate1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+    forw,
+    backw
+}
+public class rotate1 : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+            
+        
+    }
+    public bool isRotating = false;
+    public Direction direc = Direction.forw;
+	// Update is called once per frame
+	void Update () {
+        if (isRotating)
+        {
+            if (direc == Direction.forw)
+                gameObject.transform.Rotate(100 * Time.deltaTime, 0, 0);
+            else
+                gameObject.transform.Rotate(-100 * Time.deltaTime, 0, 0);
+        }
+    }
+}
